cf6cbf0a5bb798535d7c45f5367efc13e5a7d91a
GupyTheGU <nanmj1@hotmail.com>
2019-12-15 14:58:46 -0600

    S3 (1)

--- ES6/app.js
+++ ES6/app.js
@@ -47,39 +47,80 @@ const ESPACIOS = 12;
 let nombres = ["Hanna", "Mimi", "Masita"];
 let edad = ["5", "1.5", "1"];
 
-for(i in nombres){
-    let dif= ESPACIOS - nombres[i].length;
+for (i in nombres) {
+    let dif = ESPACIOS - nombres[i].length;
     console.log(nombres[i] + " ".repeat(dif) + edad[i]);
 }
 //LITERAL TEMPLATES
-function obtenTexto(){
+function obtenTexto() {
     return "no soy eloboosteada";
 }
 let textoCompleto = `El rango de ${PERSONA.nombre} es ${PERSONA.rank}  y ${obtenTexto()} asies`;
 console.log(textoCompleto);
-let miHTML=`<h1 class="${texto}"> HOLAAA</h1>`;
+let miHTML = `<h1 class="${texto}"> HOLAAA</h1>`;
 console.log(miHTML);
 
 //TEMPLATES CON TAGS
-function etiqueta(literales,...sustituciones){
-    let resultado="";
-    let i=0;
+function etiqueta(literales, ...sustituciones) {
+    let resultado = "";
+    let i = 0;
     console.log(literales);
     console.log(sustituciones);
 
-    for(let i=0;i<sustituciones.length;i++){
-        resultado+= literales[i];
-        resultado+= sustituciones[i];
+    for (let i = 0; i < sustituciones.length; i++) {
+        resultado += literales[i];
+        resultado += sustituciones[i];
     }
     return resultado;
 }
-let costo=5;
-let piezas=10;
-let ejemplo= etiqueta`${costo} lapices cuestan ${costo*piezas} pesos`;
+let costo = 5;
+let piezas = 10;
+let ejemplo = etiqueta`${costo} lapices cuestan ${costo * piezas} pesos`;
 console.log(ejemplo);
 
 //VALORES RAW
-let mensaje=`\hola \\mundo//\\s`;
-let mensaje2= String.raw `\hola \\mundo//\\s`;
+let mensaje = `\hola \\mundo//\\s`;
+let mensaje2 = String.raw`\hola \\mundo//\\s`;
 console.log(mensaje);
 console.log(mensaje2);
+//----------------------------------------------------------SECCION 3-------------------------------------------------------
+console.log("---------SECCION 3-------");
+//SE INICIALIZA EN LOS ARGUMENTOS SI SON VALORES OPCIONALES
+//LOS OPCIONALES VAN DESPUES DE LOS PERMANENTES
+function opcional(mensaje = "Holis", tiempo = 2000) {
+    //mensaje=mensaje||"Hola Mundo";
+    //mensaje = (typeof mensaje !== "undefined") ? mensaje : "Hola Mundo 2222";
+    setTimeout(
+        function () {
+            console.log(mensaje);
+        }, tiempo);
+}
+opcional();
+//PUEDEN SER FUNCIONES, OBJETOS Y MÁS 
+function masVal(fn = funcEjemplo, perrito = { nombre: "Puky" }) {
+    console.log(typeof fn);
+    if (typeof fn === "undefined") {
+        console.error("No es una función");
+        return;
+    }
+    fn();
+    console.log(perrito);
+}
+//LOS PARAMETROS OPCIONALES NO SE VEN REFLEJADOS EN "ARGUMENTS"
+function funcEjemplo() {
+    console.log("Ejemplo de funcion opcional");
+}
+masVal();
+//PARAMETROS REST
+let alumnos = ["MASITA"];
+let alumnos2;
+function agregaAlumnos(arr_alumnos, ...alumnos) {
+    console.log(arguments);
+    for (let i = 0; i < alumnos.length; i++) {
+        arr_alumnos.push(alumnos[i]);
+    }
+    return arr_alumnos;
+}
+
+alumnos2 = agregaAlumnos(alumnos, "HANNA", "MIMI", "ANORI");
+console.log(alumnos2);
